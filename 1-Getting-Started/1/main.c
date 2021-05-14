be0509e6a807a43ec32207040858fa10ed59e83c
Aryan Chaurasia <36154235+kulterryan@users.noreply.github.com>
2021-05-14 15:41:17 +0530

    first example

--- 1-Getting-Started/1/main.c
+++ 1-Getting-Started/1/main.c
@@ -1,11 +1,27 @@
+/**
+ * @file main.c
+ * @author Aryan Chaurasia
+ * @brief Simple Interest Calculation
+ * @version 0.1
+ * @date 2021-05-14
+ */
+
 #include <stdio.h>
 
 int main(){
-    int p=1000,n=3;
+    /**
+     * @brief
+     * p = principle value
+     * n = time
+     * r = rate
+     * si = simple interest  
+     */
+    int p=1000,n=3;     /* Declaration */
     float r=8.5, si;
 
+    // formula of calculation
     si = p * n * r / 100;
     printf( "%f\n", si);
     return 0;
-    
+                                                  
 }
